e2e289dbac721138c7b1cdf0aa36e2eab7cde227
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2023-02-01 22:28:21 -0600

    framework for sidebar created

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,17 +1,11 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import Link from 'next/link';
-import { signOut } from '../utils/auth';
 
 export default function NavBar() {
   return (
     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
       <div className="container-fluid">
-        <Link passHref href="/">
-          <a className="navbar-brand" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01">
-            CHANGE ME
-          </a>
-        </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
         </button>
@@ -24,10 +18,17 @@ export default function NavBar() {
                   Home
                 </a>
               </Link>
+              <Link passHref href="/">
+                <a className="nav-link">
+                  Search Bar
+                </a>
+              </Link>
+              <Link passHref href="/">
+                <a className="nav-link">
+                  Profile Image
+                </a>
+              </Link>
             </li>
-            <button type="button" className="btn btn-danger" onClick={signOut}>
-              Sign Out
-            </button>
           </ul>
         </div>
       </div>
