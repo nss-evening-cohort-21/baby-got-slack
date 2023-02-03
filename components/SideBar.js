5fb5d6ea084da8c1e24c50a26c44b7f95878042d
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2023-02-02 21:01:43 -0600

    drop down menu is an array map

--- components/SideBar.js
+++ components/SideBar.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
 function Sidebar() {
+  const users = [
+    { id: 1, name: 'Channel 1' },
+    { id: 2, name: 'Channel 2' },
+    { id: 3, name: 'Channel 3' },
+    { id: 4, name: 'Channel 4' },
+  ];
+
   return (
     <Navbar className="sidebar d-flex flex-column left-sidebar" bg="light" expand="lg">
       <Navbar.Brand href="/">Baby Got Slack</Navbar.Brand>
@@ -9,11 +16,11 @@ function Sidebar() {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="flex-column">
           <NavDropdown title="Channels" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            {users.map((user) => (
+              <NavDropdown.Item key={user.id} href={`#action/${user.id}`}>
+                {user.name}
+              </NavDropdown.Item>
+            ))}
           </NavDropdown>
         </Nav>
       </Navbar.Collapse>
