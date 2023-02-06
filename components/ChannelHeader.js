0de238086b7fa22360d1ace038a52e8fcf66abf0
Van Hoang <111828642+vhoang11@users.noreply.github.com>
2023-02-06 00:26:29 -0600

    trying to get channel name to change based on channel

--- components/ChannelHeader.js
+++ components/ChannelHeader.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { useRouter } from 'next/router';
 import ChannelHeaderForm from './forms/ChannelHeaderForm';
+import { getSingleChannel } from '../api/channelsData';
+// import { useAuth } from '../utils/context/authContext';
 
-function ChannelHeader() {
+// const initialState = {
+//   name: '',
+// };
+
+function ChannelHeader({ obj, onUpdate }) {
   // const [channelName, setChannelName] = useState({});
-  // const router = useRouter();
+  const router = useRouter();
+  const { firebaseKey } = router.query;
+  // const { user } = useAuth();
 
-  // useEffect(() => {
-  //   setChannelName(channelObj);
-  // }, [channelObj]);
+  const getChannelDetails = () => {
+    getSingleChannel(firebaseKey).then(() => onUpdate());
+  };
+
+  useEffect(() => {
+    getChannelDetails();
+    // onUpdate();
+  }, []);
 
   return (
     <div id="channel-header">
       <div className="card">
         <div className="card-body" style={{ display: 'flex', flex: 'flex-wrap' }}>
-          <h5 className="card-title">Channel Name</h5>
-          <ChannelHeaderForm />
+          <h5 className="card-title"># {obj.name}</h5>
+          <ChannelHeaderForm onUpdate={getChannelDetails} />
         </div>
       </div>
     </div>
@@ -27,9 +41,10 @@ ChannelHeader.propTypes = {
     name: PropTypes.string,
     description: PropTypes.string,
     private: PropTypes.bool,
+    starred: PropTypes.bool,
     firebaseKey: PropTypes.string,
   }).isRequired,
-  // onUpdate: PropTypes.func.isRequired,
+  onUpdate: PropTypes.func.isRequired,
 };
 
 export default ChannelHeader;
