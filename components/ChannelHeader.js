328bf282a7faf72364e5e2af368e656c20a81c22
Van Hoang <111828642+vhoang11@users.noreply.github.com>
2023-02-06 19:01:13 -0600

    name error fixed for channel header, but need to add onUpdate so it changes depending on channel you are on

--- components/ChannelHeader.js
+++ components/ChannelHeader.js
@@ -1,35 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useRouter } from 'next/router';
 import ChannelHeaderForm from './forms/ChannelHeaderForm';
 import { getSingleChannel } from '../api/channelsData';
 // import { useAuth } from '../utils/context/authContext';
 
-// const initialState = {
-//   name: '',
-// };
-
-function ChannelHeader({ obj, onUpdate }) {
-  // const [channelName, setChannelName] = useState({});
+function ChannelHeader() {
+  const [channelName, setChannelName] = useState({ name: '' });
   const router = useRouter();
   const { firebaseKey } = router.query;
   // const { user } = useAuth();
 
   const getChannelDetails = () => {
-    getSingleChannel(firebaseKey).then(() => onUpdate());
+    getSingleChannel(firebaseKey).then(setChannelName);
   };
 
   useEffect(() => {
     getChannelDetails();
-    // onUpdate();
   }, []);
 
   return (
     <div id="channel-header">
       <div className="card">
         <div className="card-body" style={{ display: 'flex', flex: 'flex-wrap' }}>
-          <h5 className="card-title"># {obj.name}</h5>
-          <ChannelHeaderForm onUpdate={getChannelDetails} />
+          <h5 className="card-title"># {channelName?.name}</h5>
+          <ChannelHeaderForm />
         </div>
       </div>
     </div>
@@ -44,7 +39,6 @@ ChannelHeader.propTypes = {
     starred: PropTypes.bool,
     firebaseKey: PropTypes.string,
   }).isRequired,
-  onUpdate: PropTypes.func.isRequired,
 };
 
 export default ChannelHeader;
