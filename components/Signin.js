809c2f20605af266b532187f3966408df89f7e63
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2023-02-11 10:38:28 -0600

    page titles, updated SignIn page

--- components/Signin.js
+++ components/Signin.js
@@ -1,22 +1,31 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import { signIn } from '../utils/auth';
 
 function Signin() {
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hi there!</h1>
-      <p>Click the button below to login!</p>
-      <button type="button" className="btn btn-primary btn-lg copy-btn" onClick={signIn}>
-        Sign In
-      </button>
+    <div style={{ backgroundColor: 'purple', color: 'white' }}>
+      <div
+        className="text-center d-flex flex-column justify-content-center align-content-center"
+        style={{
+          height: '90vh',
+          padding: '30px',
+          maxWidth: '400px',
+          margin: '0 auto',
+        }}
+      >
+        <div style={{ marginTop: '3%', marginBottom: '2%' }}>
+          <img src="https://static.tvtropes.org/trope_videos_transcoded/images/sd/s3wz9c.jpg" width="95%" height="95%" alt="baby got slack" />
+        </div>
+        <h1>Oh. My. God.</h1>
+        <h2>Becky.</h2>
+        <h3>Look at her message.</h3>
+        <h4>It is so</h4>
+        <h1>BIG.</h1>
+        <button type="button" className="btn btn-primary btn-lg copy-btn" onClick={signIn}>
+          Join The Conversation
+        </button>
+      </div>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
+import Head from 'next/head';
 
 function MainPage() {
   return (
     <>
+      <Head>
+        <title>Welcome To Baby Got Slack!</title>
+      </Head>
       <div style={{ marginTop: '3%', marginBottom: '2%' }}>
         <img src="https://media.licdn.com/dms/image/C4E22AQEc3c9vriOYhQ/feedshare-shrink_1280/0/1675472515403?e=1678924800&v=beta&t=CzMKcIQH7chCPDnXrlIKcLxHG-2UvjHCDZ0p107MTeQ" width="75%" height="75%" alt="baby got slack" />
       </div>
