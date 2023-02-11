913195a9eeb9d26e16684d4cb5aa5917135c2cf1
Mark Hamilton <ham.mark126@gmail.com>
2023-02-11 09:41:15 -0600

    added head tag in the channel page

--- pages/channel/[firebaseKey].js
+++ pages/channel/[firebaseKey].js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 import MessageForm from '../../components/forms/MessageForm';
 import ChannelHeader from '../../components/ChannelHeader';
 import MessageCard from '../../components/MessageCard';
@@ -22,25 +23,30 @@ function ViewChannel() {
 
   return (
 
-    <div className="text-center my-4">
-      <div id="channel-card">
-        <ChannelHeader />
-      </div>
+    <>
+      <Head>
+        <title>{channelDetails.name}</title>
+      </Head>
+      <div className="text-center my-4">
+        <div id="channel-card">
+          <ChannelHeader />
+        </div>
 
-      <div id="message-container">
-        {channelDetails.messages?.map((message) => (
-          <MessageCard key={message.firebaseKey} messageObj={message} onUpdate={getAllTheMessages} isMine={message.uid === user.uid} />
-        ))}
-      </div>
+        <div id="message-container">
+          {channelDetails.messages?.map((message) => (
+            <MessageCard key={message.firebaseKey} messageObj={message} onUpdate={getAllTheMessages} isMine={message.uid === user.uid} />
+          ))}
+        </div>
 
-      <div style={{
-        backgroundColor: '#F8F8F8',
-      }}
-      >
-        <MessageForm onUpdate={getAllTheMessages} />
-      </div>
+        <div style={{
+          backgroundColor: '#F8F8F8',
+        }}
+        >
+          <MessageForm onUpdate={getAllTheMessages} />
+        </div>
 
-    </div>
+      </div>
+    </>
   );
 }
 
