7cf967dabf10ab7280cc60f13913a4686a948a1b
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2023-02-10 19:47:28 -0600

    minor css changes

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ function MainPage() {
         <img src="https://media.licdn.com/dms/image/C4E22AQEc3c9vriOYhQ/feedshare-shrink_1280/0/1675472515403?e=1678924800&v=beta&t=CzMKcIQH7chCPDnXrlIKcLxHG-2UvjHCDZ0p107MTeQ" width="75%" height="75%" alt="baby got slack" />
       </div>
       <h1>
-        Welcome to Baby Got Slack!
+        WELCOME TO BABY GOT SLACK!
       </h1>
     </>
   );
